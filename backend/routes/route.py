9e1067bd97eb82ac1d7119516881a49b6b8633c6
Zeus Gano <zeusluffy10@gmail.com>
2026-01-03 22:15:51 +0800

    Added mode walking or driving

--- backend/routes/route.py
+++ backend/routes/route.py
@@ -11,15 +11,16 @@ def reroute(
     origin_lat: float,
     origin_lng: float,
     dest_lat: float,
-    dest_lng: float
+    dest_lng: float,
+    mode: str = "walking"
 ):
     url = "https://maps.googleapis.com/maps/api/directions/json"
     params = {
         "origin": f"{origin_lat},{origin_lng}",
         "destination": f"{dest_lat},{dest_lng}",
-        "mode": "walking",
+        "mode": mode,  # "walking" or "driving"
         "key": GOOGLE_MAPS_API_KEY,
     }
-
-    res = requests.get(url, params=params, timeout=5)
+    res = requests.get(url, params=params, timeout=8)
     return res.json()
+
